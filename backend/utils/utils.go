8cd063488dfb73ab8707671d5f7086bd52dee683
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-25 10:35:49 +0700

    feat(utils): Add helper function to get DB url

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -15,19 +15,11 @@ func GetPort(logger *zap.Logger) string {
 	return portString
 }
 
-package utils
-
-import (
-	"errors"
-	"go.uber.org/zap"
-	"os"
-)
-
-func GetPort(logger *zap.Logger) string {
-	portString := os.Getenv("PORT")
-	if portString == "" {
-		portString = "8080"
-		logger.Warn("Unknown portstring: setting default: 8080")
+func GetDBUrl(logger *zap.Logger) (string, error) {
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		logger.Panic("Could not find dbURL")
+		return "", errors.New("Could not find database url")
 	}
-	return portString
+	return dbUrl, nil
 }
