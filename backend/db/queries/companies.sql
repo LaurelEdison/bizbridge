7d3f5e5f436acef627e88bbcf25615e2346ccf66
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-28 14:01:36 +0700

    feat(company): Add query to create company and get by email,id

--- backend/db/queries/companies.sql
+++ backend/db/queries/companies.sql
@@ -1,6 +1,11 @@
 -- name: CreateCompany :one
+INSERT INTO companies(id, name, email, password_hash, address, description, photourl, username, created_at, updated_at)
+	VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)
+	RETURNING *;
 -- name: GetCompanyByID :one
+SELECT * FROM companies WHERE id = $1;
 -- name: GetCompanyByEmail :one
+SELECT * FROM companies WHERE email = $1;
 
 
 -- name: UpdateCompanyName :exec
