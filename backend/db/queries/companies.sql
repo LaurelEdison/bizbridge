76ea2b3d0a9757ac3770f47506f418b6ce2255d8
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-28 13:34:05 +0700

    chore(companies): Add sql scaffolding file for queries

---
+++ backend/db/queries/companies.sql
@@ -0,0 +1,19 @@
+-- name: CreateCompany :one
+-- name: GetCompanyByID :one
+-- name: GetCompanyByEmail :one
+
+
+-- name: UpdateCompanyName :exec
+-- name: UpdateCompanyAddress :exec
+-- name: UpdateCompanyDescription :exec
+-- name: UpdateCompanyPhotoUrl :exec
+-- name: UpdateCompanyUsername :exec
+
+
+
+--TODO: Implement these at some point
+
+-- name: UpdateCompanyEmail :exec
+
+-- name: UpdateCompanyPasswordHash :exec
+
