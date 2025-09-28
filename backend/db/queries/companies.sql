77756e2bebfeaeacfe0b205367f2546f748c0292
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-28 14:02:23 +0700

    feat(company): Create queries for updating company data
    - Name
    - Address
    - Photo url
    - Description
    - Username
    - Email
    - PasswordHash

--- backend/db/queries/companies.sql
+++ backend/db/queries/companies.sql
@@ -9,16 +9,43 @@ SELECT * FROM companies WHERE email = $1;
 
 
 -- name: UpdateCompanyName :exec
+UPDATE companies
+SET name = $2,
+updated_at =$3
+WHERE id = $1;
 -- name: UpdateCompanyAddress :exec
+UPDATE companies
+SET address = $2,
+updated_at =$3
+WHERE id = $1;
 -- name: UpdateCompanyDescription :exec
+UPDATE companies
+SET description = $2,
+updated_at =$3
+WHERE id = $1;
 -- name: UpdateCompanyPhotoUrl :exec
+UPDATE companies
+SET photourl = $2,
+updated_at =$3
+WHERE id = $1;
 -- name: UpdateCompanyUsername :exec
-
+UPDATE companies
+SET username = $2,
+updated_at =$3
+WHERE id = $1;
 
 
 --TODO: Implement these at some point
 
 -- name: UpdateCompanyEmail :exec
+UPDATE companies
+SET email = $2,
+updated_at =$3
+WHERE id = $1;
 
 -- name: UpdateCompanyPasswordHash :exec
+UPDATE companies
+SET password_hash = $2,
+updated_at =$3
+WHERE id = $1;
 
