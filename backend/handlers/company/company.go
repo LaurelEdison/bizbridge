517a506d41ac71f58a091f093155867ade2d3b3a
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-28 17:48:22 +0700

    feat(company): Add handlerfunc to update company details
    - name
    - description
    - address
    - username

--- backend/handlers/company/company.go
+++ backend/handlers/company/company.go
@@ -1,12 +1,14 @@
 package company
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"time"
 
 	"github.com/LaurelEdison/bizbridge/handlers"
 	"github.com/LaurelEdison/bizbridge/handlers/apiutils"
+	"github.com/LaurelEdison/bizbridge/handlers/auth"
 	"github.com/LaurelEdison/bizbridge/internal/database"
 	"github.com/LaurelEdison/bizbridge/utils"
 	"github.com/google/uuid"
@@ -81,3 +83,87 @@ func GetMe(h *handlers.Handlers) http.HandlerFunc {
 	}
 }
 
+func UpdateCompanyDetails(h *handlers.Handlers) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		claims, ok := auth.GetClaims(r.Context())
+		if !ok {
+			apiutils.RespondWithError(h.ZapLogger, w, http.StatusUnauthorized, "Invalid claims")
+			return
+		}
+		role := claims["role"].(string)
+		idstr := claims["id"].(string)
+		id, err := uuid.Parse(idstr)
+		if err != nil {
+			apiutils.RespondWithError(h.ZapLogger, w, http.StatusBadRequest, "Invalid id")
+			return
+		}
+
+		if role != "company" {
+			apiutils.RespondWithError(h.ZapLogger, w, http.StatusBadRequest, "Unexpected role")
+			return
+		}
+		type Parameters struct {
+			Name        *string `json:"name"`
+			Description *string `json:"description"`
+			Address     *string `json:"address"`
+			Username    *string `json:"username"`
+		}
+
+		params := &Parameters{}
+		decoder := json.NewDecoder(r.Body)
+		err = decoder.Decode(&params)
+		if err != nil {
+			apiutils.RespondWithError(h.ZapLogger, w, http.StatusBadRequest, "Could not decode json")
+			return
+		}
+
+		if params.Name != nil {
+			err := h.DB.UpdateCompanyName(r.Context(), database.UpdateCompanyNameParams{
+				ID:        id,
+				Name:      *params.Name,
+				UpdatedAt: time.Now(),
+			})
+			if err != nil {
+				apiutils.RespondWithError(h.ZapLogger, w, http.StatusInternalServerError, "Could not update company name")
+				return
+			}
+		}
+
+		if params.Description != nil {
+			err := h.DB.UpdateCompanyDescription(r.Context(), database.UpdateCompanyDescriptionParams{
+				ID:          id,
+				Description: sql.NullString{String: *params.Description, Valid: true},
+				UpdatedAt:   time.Now(),
+			})
+			if err != nil {
+				apiutils.RespondWithError(h.ZapLogger, w, http.StatusInternalServerError, "Could not update company description")
+				return
+			}
+		}
+
+		if params.Address != nil {
+			err := h.DB.UpdateCompanyAddress(r.Context(), database.UpdateCompanyAddressParams{
+				ID:        id,
+				Address:   *params.Address,
+				UpdatedAt: time.Now(),
+			})
+			if err != nil {
+				apiutils.RespondWithError(h.ZapLogger, w, http.StatusInternalServerError, "Could not update company address")
+				return
+			}
+		}
+
+		if params.Username != nil {
+			err := h.DB.UpdateCompanyUsername(r.Context(), database.UpdateCompanyUsernameParams{
+				ID:        id,
+				Username:  sql.NullString{String: *params.Username, Valid: true},
+				UpdatedAt: time.Now(),
+			})
+			if err != nil {
+				apiutils.RespondWithError(h.ZapLogger, w, http.StatusInternalServerError, "Could not update company username")
+				return
+			}
+		}
+		apiutils.RespondWithJSON(h.ZapLogger, w, http.StatusOK, map[string]string{"status": "updated"})
+	}
+}
