f708a6e819e530e5843c6ae9c04ec400e9b20fb7
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-30 19:59:12 +0700

    feat(chat): Add messages sql schema

---
+++ backend/db/schema/004_messages.sql
@@ -0,0 +1,14 @@
+-- +goose Up 
+CREATE TABLE messages(
+id UUID PRIMARY KEY,
+chat_room_id UUID NOT NULL REFERENCES chat_rooms(id),
+sender_id UUID NOT NULL,
+content TEXT,
+file_url TEXT,
+file_name TEXT,
+file_size TEXT,
+sent_at TIMESTAMP DEFAULT NOW(),
+is_read BOOLEAN DEFAULT false
+);
+-- +goose Down
+DROP TABLE messages;
