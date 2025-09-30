d7c3ff72dd27706b589a2e05816e2a64fcf4bd5e
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-30 20:45:36 +0700

    feat(chat): Create run loop and broadcast helper

--- backend/handlers/chat/ws/hub.go
+++ backend/handlers/chat/ws/hub.go
@@ -23,3 +23,41 @@ func NewHub() *Hub {
 	}
 }
 
+func Run(h *Hub, logger *zap.Logger) {
+	for {
+		select {
+		case client := <-h.register:
+			logger.Info("Registering client", zap.String("Chat room id", client.chatRoomID.String()), zap.String("user id", client.userID.String()))
+			if h.rooms[client.chatRoomID] == nil {
+				h.rooms[client.chatRoomID] = make(map[*Client]bool)
+			}
+			h.rooms[client.chatRoomID][client] = true
+
+		case client := <-h.unregister:
+			logger.Info("Unregistering client", zap.String("Chat room id", client.chatRoomID.String()), zap.String("user id", client.userID.String()))
+			delete(h.rooms[client.chatRoomID], client)
+			if len(h.rooms[client.chatRoomID]) == 0 {
+				delete(h.rooms, client.chatRoomID)
+			}
+		case msg := <-h.broadcast:
+			b, err := json.Marshal(msg)
+			if err != nil {
+				continue
+			}
+			logger.Info("Attempting message", zap.ByteString("json", b))
+			for c := range h.rooms[msg.ChatRoomID] {
+				select {
+				case c.send <- b:
+				default:
+					close(c.send)
+					delete(h.rooms[msg.ChatRoomID], c)
+				}
+			}
+
+		}
+	}
+}
+
+func (h *Hub) BroadcastMsg(msg Message) {
+	h.broadcast <- msg
+}
