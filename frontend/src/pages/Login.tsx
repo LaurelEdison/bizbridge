410e1d3b14afbee0944d100fa7e0e95e6e817ccb
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-03 15:00:52 +0700

    chore(todo): Switch to access+refresh login scheme

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/auth";
 import { apiFetch } from "../api/client";
-
+//TODO: Switch to access+refresh token
 export default function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
