6dde9939bc579046f7b0838e532a72115a1d6def
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-04 10:44:20 +0700

    feat(frontend): Added routers to the pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Profile from './pages/Profile.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<App />} />
+				<Route path='/profile' element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
+
+	</StrictMode >,
 )
