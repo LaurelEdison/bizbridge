014ab199c049951d5c2ac7725b191fabc4a8d9ab
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-04 10:43:47 +0700

    feat(frontend): Add temporary profile element to main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import CompanyLogin from './pages/CompanyLogin'
 import CustomerLogin from './pages/CustomerLogin'
+import Profile from './pages/Profile'
 
+//TODO: Move profile to own page, currently only for testing
 function App() {
 	return (
 		<div>
 			<CustomerLogin />
 			<CompanyLogin />
+			<Profile />
 		</div>
 	)
 }
