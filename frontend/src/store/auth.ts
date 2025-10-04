a2451b3679f8120a218dd0ad1ee29de406aa6aa3
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-04 10:40:32 +0700

    feat(frontend): Add types for customer and company

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -1,5 +1,25 @@
 import { create } from 'zustand'
 
+export type Customer = {
+	id: string;
+	name: string;
+	email: string;
+	country: string;
+	description: string | null;
+	photourl: string | null;
+}
+
+export type Company = {
+	id: string;
+	name: string;
+	email: string;
+	address: string;
+	description: string | null;
+	photourl: string | null;
+	username: string | null;
+}
+
+
 type AuthState = {
 	token: string | null;
 	setToken: (token: string | null) => void;
