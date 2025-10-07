008a1c88662a7926b628fc767692b7594048f409
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-07 16:14:02 +0700

    fix(chat): Add on delete constraints to chatrooms

--- backend/db/schema/003_chat_rooms.sql
+++ backend/db/schema/003_chat_rooms.sql
@@ -1,8 +1,8 @@
 -- +goose Up 
 CREATE TABLE chat_rooms(
 id UUID PRIMARY KEY,
-customer_id UUID NOT NULL REFERENCES customers(id),
-company_id UUID  NOT NULL REFERENCES companies(id),
+customer_id UUID NOT NULL REFERENCES customers(id) ON DELETE CASCADE,
+company_id UUID  NOT NULL REFERENCES companies(id) ON DELETE CASCADE,
 created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
 CONSTRAINT unique_buyer_seller UNIQUE (customer_id, company_id)
