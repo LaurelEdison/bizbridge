f361ad89b4e175633d5bac646867adbf950416ce
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-07 16:00:14 +0700

    feat(frontend): Add hook for using messages

---
+++ frontend/src/hook/useMessages.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { getMessages, sendMessage } from "../api/chat";
+import type { ChatMessage } from "../store/chatStore";
+
+export function useMessages(chatroomID: string) {
+	const [messages, setMessages] = useState<ChatMessage[]>([])
+	useEffect(() => {
+		getMessages(chatroomID).then(setMessages)
+	}, [chatroomID])
+	async function handleSend(content: string) {
+		await sendMessage(chatroomID, content);
+	}
+	return [messages, handleSend]
+}
