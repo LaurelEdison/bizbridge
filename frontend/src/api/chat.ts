d9c23d3b64f5c857515a8c465f7c359f89e5d84e
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-07 15:59:21 +0700

    feat(frontend): Add functions to access backend api

--- frontend/src/api/chat.ts
+++ frontend/src/api/chat.ts
@@ -1,4 +1,5 @@
 import { apiFetch } from "./client";
+import type { ChatMessage, ChatRoom } from "../store/chatStore";
 export async function createChatRoom(recipientID: string) {
 	const res = await apiFetch("/bizbridge/social/chat", {
 		method: "POST",
@@ -6,5 +7,18 @@ export async function createChatRoom(recipientID: string) {
 			recipient_id: recipientID
 		}),
 	});
-	return res
+	return res;
+}
+export async function getChatRooms(): Promise<ChatRoom[]> {
+	return apiFetch<ChatRoom[]>(`/bizbridge/social/chat`);
+}
+export async function getMessages(chatRoomID: string): Promise<ChatMessage[]> {
+	return apiFetch(`/bizbridge/social/${chatRoomID}/message`);
+}
+
+export async function sendMessage(chatRoomID: string, content: string) {
+	return apiFetch(`/bizbridge/social/${chatRoomID}/message`, {
+		method: "POST",
+		body: JSON.stringify({ content }),
+	});
 }
