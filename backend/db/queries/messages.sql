b18ebb3ae4e36c4ccb2228fa9d8bf2004d20685c
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-07 15:44:38 +0700

    feat(chat): Create db query to get messages on chat room id

--- backend/db/queries/messages.sql
+++ backend/db/queries/messages.sql
@@ -2,3 +2,6 @@
 INSERT INTO messages(id, chat_room_id, sender_id, content, sent_at)
 VALUES($1, $2, $3, $4, $5)
 RETURNING *;
+
+-- name: GetMessages :many
+SELECT * FROM messages WHERE chat_room_id = $1;
