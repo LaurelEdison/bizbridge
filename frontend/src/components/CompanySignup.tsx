96be6b217140844f2b9ca0b0393f97554e6bfbd8
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-08 13:55:57 +0700

    feat(frontend): Create company signup component

---
+++ frontend/src/components/CompanySignup.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/auth";
+import { apiFetch } from "../api/client";
+import type { Company } from "../store/auth";
+//TODO: Switch to access+refresh token
+export default function CompanySignup() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [address, setAddress] = useState("");
+	const setToken = useAuthStore((s) => s.setToken);
+	async function handleCompanyLogin(e: React.FormEvent) {
+		e.preventDefault();
+		try {
+			const company = await apiFetch<Company>("/bizbridge/company/login", {
+				method: "POST",
+				body: JSON.stringify({ name, email, password, address }),
+			});
+			const data = await apiFetch<{ token: string }>("/bizbridge/company/login", {
+				method: "POST",
+				body: JSON.stringify({ email, password }),
+			});
+			setToken(data.token);
+			console.log(data.token);
+
+			const role = "company";
+			useAuthStore.getState().setRole(role);
+			useAuthStore.getState().setCompany(company);
+
+		} catch (err) {
+			console.error("Login failed", err);
+		}
+	}
+	return (
+		<form onSubmit={handleCompanyLogin} className="flex flex-col gap-4 w-full max-w-sm">
+			<input
+				className="border p-2"
+				placeholder="Name"
+				value={name}
+				onChange={(s) => setName(s.target.value)}
+			/>
+			<input
+				className="border p-2"
+				placeholder="email"
+				value={email}
+				onChange={(s) => setEmail(s.target.value)}
+			/>
+			<input
+				className="border p-2"
+				placeholder="Password"
+				type="password"
+				value={password}
+				onChange={(s) => setPassword(s.target.value)}
+			/>
+			<input
+				className="border p-2"
+				placeholder="Address"
+				value={address}
+				onChange={(s) => setAddress(s.target.value)}
+			/>
+			<button className="bg-blue-500 text-white p-2 rounded" type="submit">
+				Sign up!
+			</button>
+		</form>
+	);
+}
+
