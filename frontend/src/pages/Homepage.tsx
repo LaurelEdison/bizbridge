52712eefb734e4e487430e8b0cbb0f52409f9caa
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-11 21:27:56 +0700

    feat(frontend): Refine css for pages

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import { Info } from "../components/Info";
 
 export function HomePage() {
 	return (
-		<div className="min-h-screen flex flex-col">
+		<div className="min-h-screen flex flex-col bg-gradient-to-b from-blue-50 to-white">
 			<Navbar />
 			<Info />
 		</div>
