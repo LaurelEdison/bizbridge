4430ce396adcdfdd88e5efed38f3bd94d2b8428e
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-20 18:42:46 +0700

    feat(files): Add queries for company files

---
+++ backend/db/queries/company_files.sql
@@ -0,0 +1,10 @@
+-- name: CreateCompanyFile :one
+INSERT INTO company_files (id, company_id, category, file_name, url, uploaded_at) 
+VALUES ( $1, $2, $3, $4, $5, $6)
+RETURNING *;
+
+-- name: DeleteCompanyFile :exec
+DELETE FROM company_files WHERE id = $1;
+
+-- name: GetCompanyFilesFromCompanyID :many
+SELECT * FROM company_files WHERE company_id = $1;
