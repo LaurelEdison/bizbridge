d1a1e04ec5cfffe0143c356a45c61e43a08d69a1
EdisonLaurel <arel.ferdinand@gmail.com>
2025-10-21 21:09:10 +0700

    feat(frontend): Add carousel speed

---
+++ frontend/src/hook/CarouselSetting.ts
@@ -0,0 +1,18 @@
+import { Settings } from "react-slick";
+
+const carouselSettings: Settings = {
+	dots: true,
+	infinite: true,
+	fade: true,
+	speed: 800,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	arrows: false,
+	pauseOnHover: true,
+	cssEase: "linear",
+	swipe: true,
+};
+
+export default carouselSettings;
