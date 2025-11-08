b6c4e7ebc11fed6cff7a37cffc4313914b049391
EdisonLaurel <arel.ferdinand@gmail.com>
2025-11-08 17:20:34 +0700

    fix(frontend): Use new api link for websockets

--- frontend/src/hook/useWebsocket.ts
+++ frontend/src/hook/useWebsocket.ts
@@ -1,12 +1,13 @@
 import { useEffect, useRef } from "react";
 import { useAuthStore } from "../store/auth";
+const baseUrl = import.meta.env.VITE_WS_API_URL || "http://localhost:8080";
 
 export function useWebsocket(chatroomID: string, onMessage: (msg: any) => void) {
 	const wsRef = useRef<WebSocket | null>(null)
 	const { token } = useAuthStore();
 	useEffect(() => {
 		if (!token) return;
-		const ws = new WebSocket(`ws://localhost:8080/bizbridge/social/${chatroomID}?token=${token}`);
+		const ws = new WebSocket(`${baseUrl}/social/${chatroomID}?token=${token}`);
 		ws.onmessage = (event) => {
 			const data = JSON.parse(event.data)
 			onMessage(data.payload);
