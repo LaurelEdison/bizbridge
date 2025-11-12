d7437f69c73de678e8e95a5b3decde7b6aeaf1c3
EdisonLaurel <arel.ferdinand@gmail.com>
2025-11-12 16:08:04 +0700

    fix(frontend): Make search button have more consistent color scheme

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -47,7 +47,7 @@ export function SearchPage() {
 					</div>
 					<button
 						onClick={handleSearch}
-						className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+						className="bg-[#198754] text-white px-4 py-2 rounded hover:bg-blue-600 transition"
 					>
 						Search
 					</button>
